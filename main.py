419941ab9af2362e8fdef694fc2728871422a6cd
JitingSu <2859275868@qq.com>
2025-01-19 18:19:36 +0800

    路径修改

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ MAX_LENGTH = 128
 NUM_CLASSES = 3
 
 # 数据和模型路径
-DATA_DIR = r"e:\Learning_material\junior\AI\5_FinalLab\dataset"
+# DATA_DIR = r"e:\Learning_material\junior\AI\5_FinalLab\dataset"
+DATA_DIR = "/kaggle/input/project5/dataset"
+
 TRAIN_FILE = os.path.join(DATA_DIR, "train.txt")
 TEST_FILE = os.path.join(DATA_DIR, "test_without_label.txt")
 IMG_DIR = os.path.join(DATA_DIR, "data")
